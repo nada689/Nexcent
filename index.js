1ca250bb7f375019b99ee1766bcab52aa13bcbd9
nada689 <120287148+nada689@users.noreply.github.com>
2024-03-26 01:56:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+//Start Nada's section
+
+// Start responsive menu function
+
+function myFunction() {
+  var x = document.getElementById("header_r");
+  if (x.className === "header_right") {
+    x.className += " responsive";
+    y.className.replace(" active", "hidden") ;
+  } else {
+    x.className = "header_right";
+  }
+}
+
+// End responsive menu function
+
+//Start Slideshow container 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("my_slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+//End of Slideshow Container
+
+//End Nada's  section
 
